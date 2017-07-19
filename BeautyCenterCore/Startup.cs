49c6eabcf6c6c0dbe699b237a4d1633f7d815ee6
robert antonio frias de leon <chico_3_13@hotmail.com>
2017-07-19 12:59:38 -0400

    Vamos haya

--- BeautyCenterCore/Startup.cs
+++ BeautyCenterCore/Startup.cs
@@ -82,7 +82,7 @@ namespace BeautyCenterCore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
